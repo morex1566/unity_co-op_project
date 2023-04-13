9c534f0089c48eea9f9c5bccfc47ceeee469e02b
Eon_2e <43786927+shoong2@users.noreply.github.com>
2023-04-13 17:26:50 +0900

    [2023-04-13]mesh cut 추가

--- Project/Assets/Scripts/Eon/PlayerAction.cs
+++ Project/Assets/Scripts/Eon/PlayerAction.cs
@@ -7,23 +7,42 @@ public class PlayerAction : MonoBehaviour
     [SerializeField]
     float moveSpeed=0.5f;
 
+
+    public Transform cuttingPlane;
+    public LayerMask cuttableLayer;
+
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+
     // Update is called once per frame
     void Update()
     {
-        // float Horizontal = Input.GetAxis("Horizontal");
-        // Vector3 Position = transform.position;
-        // Position.x += -1*Horizontal * moveSpeed * Time.deltaTime;
-        //
-        // transform.position = Position;
+        float Horizontal = Input.GetAxis("Horizontal");
+        Vector3 Position = transform.position;
+        Position.x += -1 * Horizontal * moveSpeed * Time.deltaTime;
+
+        transform.position = Position;
     }
 
-    private void OnTriggerEnter(Collider other)
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Block")
+    //    {
+
+    //        Debug.Log("hi");
+    //        //GameObject[] gameObjects = MeshCut.Cut(other.gameObject, transform.position, Vector3.down, other.gameObject.GetComponent<MeshRenderer>().material);
+    //        GameManager.Instance.HealthCheck();
+    //    }
+    //}
+
+    private void Start()
     {
-        if (other.tag == "Block")
-        {
-            Debug.Log("hi");
-            //GameObject[] gameObjects = MeshCut.Cut(other.gameObject, transform.position, Vector3.down, other.gameObject.GetComponent<MeshRenderer>().material);
-            GameManager.Instance.HealthCheck();
-        }
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
     }
 }
