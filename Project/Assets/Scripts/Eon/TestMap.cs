84a11a40a2cd2666b684b6cc7dd22d7e0e10314a
Eon_2e <43786927+shoong2@users.noreply.github.com>
2023-05-01 18:29:23 +0900

    [2023-05-01] meshCut 적용

--- Project/Assets/Scripts/Eon/TestMap.cs
+++ Project/Assets/Scripts/Eon/TestMap.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class TestMap : MonoBehaviour
 {
+    Animator anim;
 
-    public GameObject map;
-    void Start()
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag =="Fragile Obstacle")
+        {
+            Renderer renderer = collision.gameObject.GetComponent<Renderer>();
+            Material material = renderer.materials[0];
+            MeshCut.Cut(collision.gameObject, collision.transform.position, Vector3.right, material);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("LeftAttack");
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("RightAttack");
+        }
     }
 }
