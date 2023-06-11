d9fee491291d3dbddad040573599a8953a060878
Jiwon <morex1566@gmail.com>
2023-06-11 22:35:57 +0900

    Revert "Revert "Merge branch 'debug_test' into debugServer_test""
    This reverts commit 8f686460a436af33a409e8175942df2045a21f60.

--- Project/Assets/Scripts/Eon/ObjectSpawner.cs
+++ Project/Assets/Scripts/Eon/ObjectSpawner.cs
@@ -9,6 +9,10 @@ public class ObjectSpawner : MonoBehaviour
     [SerializeField]
     GameObject player;
 
+    [SerializeField]
+    TutorialManager manager;
+
+
     private void Start()
     {
         StartCoroutine(MakeObject());
@@ -18,6 +22,9 @@ public class ObjectSpawner : MonoBehaviour
     {
         GameObject box1 = Instantiate(box, new Vector3(player.transform.position.x+1, 1f, -15f), Quaternion.identity);
         box1.GetComponent<BoxCollider>().enabled = true;
+        GameObject arrow = box1.transform.GetChild(0).gameObject;
+        arrow.transform.localScale = new Vector3(arrow.transform.localScale.x * -1, arrow.transform.localScale.y, arrow.transform.localScale.z);
+        yield return new WaitUntil(()=> box1.GetComponent<Object>().boxStop && manager.left);
         GameObject box2 = Instantiate(box, new Vector3(player.transform.position.x - 1, 1f, -15f), Quaternion.identity);
         box2.GetComponent<BoxCollider>().enabled = true;
         yield return null;
